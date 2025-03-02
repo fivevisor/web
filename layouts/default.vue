9e84daf0bf7a376c954b57ec7740572a62816e46
COOL <yigitgurses2012@gmail.com>
2025-03-02 13:09:27 +0300

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <h1>Test Project</h1>
+        <div>
+            <h1>Test Project</h1>
+            <div>
+                <NuxtLink to="/">Home</NuxtLink>
+                <NuxtLink to="/view">View</NuxtLink>
+            </div>
+        </div>
         <div>
             <slot />
         </div>
