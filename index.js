583660640a15d838062a06e4294a69555fb0e576
Fivevisor Manager <fivevisor@gmail.com>
2025-03-20 01:08:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 3000
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express!')
+    res.send('Hello World!')
 })
 app.get('/user/:name', (req, res) => {
     res.send(`Hello, ${req.params.name}!`)
